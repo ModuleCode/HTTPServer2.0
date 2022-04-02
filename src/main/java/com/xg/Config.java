c49cd27054e8be04d6d4dc4c81c86ea38e337636
ModuleCode <46812028+ModuleCode@users.noreply.github.com>
2022-04-02 18:10:04 +0800

    Delete src/main/java/com/xg directory

--- src/main/java/com/xg/Config.java
+++
@@ -1,9 +0,0 @@
-package com.xg;
-
-import java.util.HashMap;
-
-public class Config {
-    //项目与路径的映射关系
-    private HashMap<String,String> mapping=new HashMap<>();
-    
-}
--- src/main/java/com/xg/HTTPServerTCP.java
+++
@@ -1,9 +0,0 @@
-package com.xg;
-
-
-public class HTTPServerTCP {
-
-
-    //1.读取xml配置文件
-    //2.使用类加载器读取Servlet
-}
